b351db28c45b915099e20cca262bd06d8d60d12f
Jack <fawthorp878@gmail.com>
2022-05-12 20:46:47 +1200

    Competitive Programming Template

---
+++ file.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+
+int tempInt;
+long long tempLong;
+string tempString;
+char tempChar;
+
+
+// DP
+//vector<vector<int>> dp2d(row_count, vector<int>(column_count, initial_value));
+//vector<vector<int>> dp2d(100, vector<int>(100, -1));
+//vector<int> dp1d(col_count, initial_value);
+//vector<int> dp1d(100,-1);
+
+int main()
+{
+
+}
