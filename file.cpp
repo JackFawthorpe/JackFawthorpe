3cc4aab6d07c156db82bf825d54ae2d5dbaabb91
Jack <fawthorp878@gmail.com>
2022-05-14 12:27:02 +1200

    Update file.cpp

--- file.cpp
+++ file.cpp
@@ -11,7 +11,9 @@ int tempInt;
 long long tempLong;
 string tempString;
 char tempChar;
-
+int maxi = INT_MIN;
+int mini = INT_MAX;
+int caseNum = 1;
 
 // DP
 //vector<vector<int>> dp2d(row_count, vector<int>(column_count, initial_value));
@@ -19,7 +21,17 @@ char tempChar;
 //vector<int> dp1d(col_count, initial_value);
 //vector<int> dp1d(100,-1);
 
+bool isOdd(int number)
+{
+	return number % 2 != 0;
+}
+
+// False implies a swap, True implies correct
+bool compare(int lhs, int rhs)
+{
+}
+
 int main()
 {
-
+	
 }
